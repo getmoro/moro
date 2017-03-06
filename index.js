26951486662c5a541bdb0662b4bcfdec87745874
Nick Okapi <7697632+albacoretuna@users.noreply.github.com>
2017-03-06 13:29:32 +0200

    Use caporal for cli functionality

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 #!/usr/bin/env node
-const moment = require('moment');
-const yargs = require('yargs')
 
-yargs
-  .command({
-    command: 'hi',
-    desc: 'set start of work day',
-    handler: (argv) => {
-      console.log('Hi! your start of the day is set as: ', moment().format('HH:MM'));
-    },
-    usage: 'timo hi',
+// good name for this? 9 to 5
+const moment = require('moment')
+const prog = require('caporal')
+const VERSION = require('./package.json').version
+
+prog
+  .version(VERSION)
+  .command('hi', 'Set your start of the day, default is now!')
+  .argument('[start]', 'Specify start time if not now', /^\d\d:\d\d$/)
+  .action((args, options, logger) => {
+    console.log('Your start of the day registered as ', moment().format('HH:mm'))
   })
-  .help()
-  .wrap(72)
-  .argv;
+  .command('bye', 'Sets your end of the day time')
+  .argument('[end]', 'Specify the end of working hours if not now', /^\d\d:\d\d$/)
+  .action((args, options, logger) => {
+    const end = args.end || moment().format('HH:mm')
+    console.log('Your end of the day registered as: ', end)
+  })
+  .command('break', 'Adds to the duration of unpaid break, in minutes. A default of 30 minutes is already added. Use negative numbers to reduce the break time.')
+  .argument('[duration]', 'Specify the duration of break in minutes ', /^[\d]+$/)
+  .action((args, options, logger) => {
+    const duration = args.duration || 30
+    console.log('Break took: ', duration, 'Minutes', ' And will be removed from your work hours')
+  })
+
+prog.parse(process.argv)
+
+// ./index.js hi
+// ./index.js hi 08:23
+// ./index.js bye
