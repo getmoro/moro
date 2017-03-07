64889e3285c571ddf6373792f639a6573a5975b0
Nick Okapi <7697632+albacoretuna@users.noreply.github.com>
2017-03-07 12:11:56 +0200

    Add calculation of work hours

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 const moment = require('moment')
 const prog = require('caporal')
 const VERSION = require('./package.json').version
-const dbHelpers = require('./storage.js')
+const db = require('./storage.js')
 const TODAY = moment().format('YYYY-MM-DD')
 
 // default break time in minutes
@@ -17,32 +17,35 @@ const BREAK_DEFAULT = 30
 // ./index.js bye 17:30
 // ./index.js break 32
 // ./index.js break -32
+
+// Todo: would be smarter by to set hi, bye without any hassle.
+// First one hi, next one bye
 prog
   .version(VERSION)
   .command('hi', 'Set your start of the day, default is now!')
   .argument('[start]', 'Specify start time if not now', /^\d\d:\d\d$/)
   .action((args, options, logger) => {
     const start = args.start || moment().format('HH:mm')
-    console.log('Your start of the day registered as ', moment().format('HH:mm'))
-    dbHelpers.updateDatabase(TODAY, start, null, BREAK_DEFAULT, 'setStart')
+    console.log('Your start of the day registered as ', start)
+    db.updateDatabase(TODAY, start, null, BREAK_DEFAULT, 'setStart')
   })
   .command('bye', 'Sets your end of the day time')
   .argument('[end]', 'Specify the end of working hours if not now', /^\d\d:\d\d$/)
   .action((args, options, logger) => {
     const end = args.end || moment().format('HH:mm')
     console.log('Your end of the day registered as: ', end)
-    dbHelpers.updateDatabase(TODAY, null, end, BREAK_DEFAULT, 'setEnd')
+    db.updateDatabase(TODAY, null, end, BREAK_DEFAULT, 'setEnd')
   })
   .command('break', 'Adds to the duration of unpaid break, in minutes. A default of 30 minutes is already added. Use negative numbers to reduce the break time.')
   .argument('[duration]', 'Specify the duration of break in minutes ', /^[\d]+$/)
   .action((args, options, logger) => {
     const duration = args.duration || 30
     console.log('Break took: ', duration, 'Minutes', ' And will be removed from your work hours')
-    dbHelpers.updateDatabase(TODAY, null, null, duration, 'setBreakDuration')
+    db.updateDatabase(TODAY, null, null, duration, 'setBreakDuration')
   })
   .command('report', 'Set your start of the day, default is now!')
   .action((args, options, logger) => {
-    dbHelpers.readData(TODAY)
+    db.calculateWorkHours(TODAY)
   })
 
 prog.parse(process.argv)
