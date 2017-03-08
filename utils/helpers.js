f23dcc652c2f0c7814b12a894fb4ff8e42bf4560
Omid Hezaveh <omid.hezaveh@futurice.com>
2017-03-08 11:06:07 +0200

    Make report output more readable

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,9 @@
 // packages
 const moment = require('moment')
 
+// ours
+const logger = require('./logger.js')
+
 // input 'HH:mm', output moment object
 const composeDateObject = (timeString) => {
   const hour = timeString.split(':')[0]
@@ -8,6 +11,16 @@ const composeDateObject = (timeString) => {
   return moment({ hour, minutes })
 }
 
+const printSingleDayReport = (record) => {
+  logger.info(` \n
+  Start:        ${record.start}
+  End:          ${record.end}
+  Total break: ${record.breakDuration}(minutes)
+  Date:         ${record.date}
+  `)
+}
+
 module.exports = {
-  composeDateObject
+  composeDateObject,
+  printSingleDayReport
 }
