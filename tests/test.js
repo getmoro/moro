7df09184b97a18e91df4f1150cbe4c009ef82a6a
Mario Schroeder <mario.schroeder@springernature.com>
2017-03-23 10:28:55 +0100

    extended test

--- tests/test.js
+++ tests/test.js
@@ -8,28 +8,40 @@ import {
 
 const moment = require('moment')
 
-const sampleDayRecord = {
-  id: 1,
-  date: '2017-03-10',
-  start: '05:15',
-  end: '05:15',
-  breakDuration: 30,
-  notes: [ { id: 1, date: '2017-03-10', createdat: '06:29', note: 'hello' } ],
-  dayReport: '0 Hours and -30 Minutes'
+const diff = (start, end) => {
+  return moment.duration(moment(end).diff(moment(start)))
 }
 
-const sampleFullReprotRecord = [{
-  date: '2017-03-10',
-  workHours: moment.duration(moment('2017-03-10 17:10').diff(moment('2017-03-10 09:15')))
-}, {
-  date: '2017-03-11',
-  workHours: moment.duration(moment('2017-03-11 17:10').diff(moment('2017-03-11 09:15')))
-}]
-
 test('singleDayReport runs without crashing', t => {
+  const sampleDayRecord = {
+    id: 1,
+    date: '2017-03-10',
+    start: '05:15',
+    end: '05:15',
+    breakDuration: 30,
+    notes: [ { id: 1, date: '2017-03-10', createdat: '06:29', note: 'hello' } ],
+    dayReport: '0 Hours and -30 Minutes'
+  }
   t.pass(printSingleDayReport(sampleDayRecord))
 })
 
 test('printAllDaysReport runs without crashing', t => {
-  t.pass(printAllDaysReport(sampleFullReprotRecord))
+  const reprotRecord = [{
+    date: '2017-03-10',
+    workHours: diff('2017-03-10 09:15', '2017-03-10 17:10')
+  }]
+  t.pass(printAllDaysReport(reprotRecord))
+})
+
+test('printAllDaysReport for two runs without crashing', t => {
+  const reprotRecord = [{date: '2017-03-09', workHours: diff('2017-03-09 09:15', '2017-03-09 17:10')},
+    {date: '2017-03-10', workHours: diff('2017-03-10 09:15', '2017-03-10 17:10')}]
+  t.pass(printAllDaysReport(reprotRecord))
+})
+
+test('printAllDaysReport with week change runs without crashing', t => {
+  const reprotRecord = [{date: '2017-03-09', workHours: diff('2017-03-09 09:15', '2017-03-09 17:10')},
+    {date: '2017-03-10', workHours: diff('2017-03-10 09:15', '2017-03-10 17:10')},
+    {date: '2017-03-14', workHours: diff('2017-03-14 09:10', '2017-03-14 17:10')}]
+  t.pass(printAllDaysReport(reprotRecord))
 })
