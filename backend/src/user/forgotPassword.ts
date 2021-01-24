8acf4f6bb3ea33db2f1c3bd52cc253ea845fa2a1
Pouria Maleki <dev.maleki@gmail.com>
2021-01-24 13:34:46 +0200

    check for recent tokens first

--- backend/src/user/forgotPassword.ts
+++ backend/src/user/forgotPassword.ts
@@ -1,9 +1,13 @@
 import { customAlphabet } from 'nanoid';
 import { TOKEN_TYPES } from '@prisma/client';
 import { MutationResolvers } from '../graphql/resolvers-types';
-import { NUMBERS, TOKEN_SIZE } from '../utils/constants';
+import { NUMBERS, TOKEN_SIZE, TOKEN_EXPIRE_MINUTES } from '../utils/constants';
 
 const makePasswordToken = customAlphabet(NUMBERS, TOKEN_SIZE);
+const getRecentTime = (min: number): Date => {
+  const now = new Date();
+  return new Date(now.getTime() - min * 60000);
+};
 
 export const forgotPassword: MutationResolvers['forgotPassword'] = async (
   parent,
@@ -11,7 +15,29 @@ export const forgotPassword: MutationResolvers['forgotPassword'] = async (
   { prisma },
 ) => {
   // check if recently used one
-  // prisma.token.find
+  const recentTokens = await prisma.token.findMany({
+    where: {
+      email: credentials.email,
+      type: TOKEN_TYPES.RESET_PASSWORD,
+      resolved: false,
+      createdAt: {
+        gte: getRecentTime(TOKEN_EXPIRE_MINUTES / 2),
+      },
+    },
+    select: {
+      token: true,
+      createdAt: true,
+    },
+  });
+  const lastToken = recentTokens.pop()?.token;
+
+  // if recently used a valid token, email it again
+  if (lastToken) {
+    // email the token
+    console.log('Reset password token: ', lastToken);
+
+    return { success: true };
+  }
 
   const user = await prisma.user.findUnique({
     where: { email: credentials.email },
--- backend/src/utils/constants.ts
+++ backend/src/utils/constants.ts
@@ -1,4 +1,5 @@
 export const NUMBERS = '0123456789';
 export const TOKEN_SIZE = 6;
+export const TOKEN_EXPIRE_MINUTES = 10;
 export const JWT_SECRET = process.env.JWT_SECRET || 'JWT_DARK_SECRET';
 export const JWT_ALGORITHM = 'HS256';
