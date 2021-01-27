e6f63dc466d5f12bf97c0a36649cc9c67849bbde
Pouria Maleki <dev.maleki@gmail.com>
2021-01-28 00:13:13 +0200

    reset password with a link

--- backend/src/user/forgotPassword.ts
+++ backend/src/user/forgotPassword.ts
@@ -1,15 +1,11 @@
-import { customAlphabet } from 'nanoid';
+import { v4 as uuidv4 } from 'uuid';
 import { TOKEN_TYPES } from '@prisma/client';
 import { MutationResolvers } from '../graphql/resolvers-types';
 import { getRecentTime } from '../utils/getRecentTime';
-import { TOKEN_EXPIRE_MINUTES } from '../utils/constants';
+import { CLIENT_ADDRESS, TOKEN_EXPIRE_MINUTES } from '../utils/constants';
 
-const NUMBERS = '0123456789';
-const TOKEN_SIZE = 6;
 const DAY = 24 * 60; // a day is 24h * 60min
 const ATTEMPTS_PER_DAY_LIMIT = 3;
-
-const makePasswordToken = customAlphabet(NUMBERS, TOKEN_SIZE);
 
 export const forgotPassword: MutationResolvers['forgotPassword'] = async (
   parent,
@@ -27,8 +23,10 @@ export const forgotPassword: MutationResolvers['forgotPassword'] = async (
       },
     },
     select: {
+      id: true,
       token: true,
       createdAt: true,
+      updatedAt: true,
     },
   });
 
@@ -37,11 +35,26 @@ export const forgotPassword: MutationResolvers['forgotPassword'] = async (
     return { success: false, message: 'Too many requests' };
   }
 
-  // if recently used a valid token and there is still time to use it, email it again
   const lastTokenRow = recentTokenRows.pop();
+
+  // don't sent new emails if user just requested one
+  if (lastTokenRow && lastTokenRow.updatedAt > getRecentTime(1)) {
+    return { success: true, message: 'Wait a minute before asking for a new email' };
+  }
+
+  // if recently used a valid token and there is still time to use it, email it again
   if (lastTokenRow && lastTokenRow.createdAt > getRecentTime(TOKEN_EXPIRE_MINUTES / 2)) {
+    // update the token timestamp, to be able to check when we sent last email
+    await prisma.token.update({
+      where: { id: lastTokenRow.id },
+      data: { resolved: false },
+    });
+
     // email the token
-    console.log('Reset password token: ', lastTokenRow.token);
+    console.log(
+      'email resend: Reset password token: ',
+      `${CLIENT_ADDRESS}/resetPassword?token=${lastTokenRow.token}`,
+    );
 
     return { success: true };
   }
@@ -52,12 +65,12 @@ export const forgotPassword: MutationResolvers['forgotPassword'] = async (
   });
 
   if (!user) {
-    // Here we are not going to email anything, but
+    // Here we are not going to send an email, but
     // shouldn't tell user if the email was not exists to prevent identifiying registered emails by strangers
     return { success: true };
   }
 
-  const token = makePasswordToken();
+  const token = uuidv4();
   await prisma.token.create({
     data: {
       user: { connect: { email: credentials.email } },
@@ -66,8 +79,11 @@ export const forgotPassword: MutationResolvers['forgotPassword'] = async (
     },
   });
 
-  // email the token
-  console.log('Reset password token: ', token);
+  // email the reset password link
+  console.log(
+    'email: Reset password token: ',
+    `${CLIENT_ADDRESS}/resetPassword?token=${token}`,
+  );
 
   return { success: true };
 };
--- backend/src/user/hashUserPassword.ts
+++ backend/src/user/hashUserPassword.ts
@@ -3,11 +3,11 @@ import bcrypt from 'bcrypt';
 // more rounds means slower hashing but higher security
 const saltRounds = 10;
 
-type ObjectWithPassword = {
-  password?: string | null;
-};
+interface IObjectWithPassword {
+  password: string;
+}
 
-export const hashUserPassword = async <T extends ObjectWithPassword>(
+export const hashUserPassword = async <T extends IObjectWithPassword>(
   user: T,
 ): Promise<T> => {
   if (user.password) {
